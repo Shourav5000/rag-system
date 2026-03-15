926e98818e5737798638d1478b571bf54e0ba2a3
Shourav Mandal <shourav.mandal@gmail.com>
2026-03-15 16:34:12 -0400

    Fix imports and lightweight embeddings

--- rag_api.py
+++ rag_api.py
@@ -1,5 +1,7 @@
 import os
 import tempfile
+import hashlib
+import math
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -10,8 +12,6 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_anthropic import AnthropicEmbeddings
 
 load_dotenv()
 
@@ -24,30 +24,23 @@ llm = ChatAnthropic(
     max_tokens=1024
 )
 
-# Lightweight embeddings using Anthropic
 class SimpleEmbeddings:
     def embed_documents(self, texts):
-        return self._get_embeddings(texts)
+        return [self._embed(t) for t in texts]
 
     def embed_query(self, text):
-        return self._get_embeddings([text])[0]
+        return self._embed(text)
 
-    def _get_embeddings(self, texts):
-        import hashlib
-        import math
-        embeddings = []
-        for text in texts:
-            hash_val = hashlib.md5(text.encode()).hexdigest()
-            vec = []
-            for i in range(0, min(len(hash_val), 64), 2):
-                val = int(hash_val[i:i+2], 16) / 255.0
-                vec.append(val)
-            while len(vec) < 384:
-                seed = len(vec)
-                val = (math.sin(seed * 12.9898 + len(text) * 78.233) * 43758.5453) % 1.0
-                vec.append(abs(val))
-            embeddings.append(vec[:384])
-        return embeddings
+    def _embed(self, text):
+        hash_val = hashlib.md5(text.encode()).hexdigest()
+        vec = []
+        for i in range(0, len(hash_val) - 1, 2):
+            vec.append(int(hash_val[i:i+2], 16) / 255.0)
+        while len(vec) < 384:
+            seed = len(vec)
+            val = abs((math.sin(seed * 12.9898 + len(text) * 78.233) * 43758.5453) % 1.0)
+            vec.append(val)
+        return vec[:384]
 
 embeddings = SimpleEmbeddings()
 
